a4f09be3a68c5dea634e7c37db05e620a1029592
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-09-14 07:58:02 +0200

    fixing indentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,38 @@
 #include <opencv2/opencv.hpp>
 
-cv::Mat computeNaive(cv::Mat &image){
+cv::Mat computeNaive(cv::Mat& image)
+{
     cv::Mat iimage = cv::Mat::zeros(image.rows, image.cols, CV_64FC1);
-    for(int x = 0; x < image.rows; x++){
-        for(int y = 0; y < image.cols; y++){
-            for (int ix = 0; ix <= x; ix++){
-                for (int iy = 0; iy <= y; iy++){
+    for (int x = 0; x < image.rows; x++) {
+        for (int y = 0; y < image.cols; y++) {
+            for (int ix = 0; ix <= x; ix++) {
+                for (int iy = 0; iy <= y; iy++) {
                     iimage.at<double>(x, y) += image.at<double>(ix, iy);
-                } }
-        } }
+                }
+            }
+        }
+    }
     return iimage;
 }
 
-
-cv::Mat computeUsingPrev(cv::Mat &image){
+cv::Mat computeUsingPrev(cv::Mat& image)
+{
     cv::Mat iimage = cv::Mat::zeros(image.rows, image.cols, CV_64FC1);
 
-    int stride=iimage.cols;
-    auto *image_ptr = image.ptr<double>(0);
+    int stride = iimage.cols;
+    auto* image_ptr = image.ptr<double>(0);
 
-    int istride=image.cols;
-    auto *iimage_ptr = iimage.ptr<double>(0);
+    int istride = image.cols;
+    auto* iimage_ptr = iimage.ptr<double>(0);
 
-    for(int x = 0; x < image.rows; x++){
+    for (int x = 0; x < image.rows; x++) {
         int nh = x - 1;
-        double yxSum=0;
-        for(int y = 0; y < image.cols; y++){
-            if (nh >= 0){
-                iimage_ptr[y] += iimage_ptr[y - stride]; }yxSum+= image_ptr[y];
+        double yxSum = 0;
+        for (int y = 0; y < image.cols; y++) {
+            if (nh >= 0) {
+                iimage_ptr[y] += iimage_ptr[y - stride];
+            }
+            yxSum += image_ptr[y];
             iimage_ptr[y] += yxSum;
         }
         iimage_ptr += stride;
@@ -36,19 +41,19 @@ cv::Mat computeUsingPrev(cv::Mat &image){
     return iimage;
 }
 
-
-cv::Mat computeUsingPadding(cv::Mat &image){
+cv::Mat computeUsingPadding(cv::Mat& image)
+{
     cv::Mat iimage = cv::Mat::zeros(image.rows + 1, image.cols + 1, CV_64FC1);
 
-    int istride=iimage.cols;
-    auto *iimage_ptr = iimage.ptr<double>(1) + 1; //Start from (1,1)
+    int istride = iimage.cols;
+    auto* iimage_ptr = iimage.ptr<double>(1) + 1; //Start from (1,1)
 
-    int stride=image.cols;
-    auto *image_ptr = image.ptr<double>(0);
+    int stride = image.cols;
+    auto* image_ptr = image.ptr<double>(0);
 
-    for(int x = 0; x < image.rows; x++){
+    for (int x = 0; x < image.rows; x++) {
         int ySum = 0;
-        for(int y = 0; y < image.cols; y++){
+        for (int y = 0; y < image.cols; y++) {
             ySum += (int)image_ptr[y];
             iimage_ptr[y] = ySum + iimage_ptr[y - istride];
         }
@@ -59,8 +64,8 @@ cv::Mat computeUsingPadding(cv::Mat &image){
     return iimage;
 }
 
-
-int main() {
+int main()
+{
     cv::TickMeter timer;
     cv::Mat sample = cv::Mat::eye(200, 200, CV_64FC1);
 
@@ -80,7 +85,6 @@ int main() {
     computeUsingPadding(sample);
     timer.stop();
     std::cout << "computeUsingPadding (200*200): " << timer << std::endl;
-
 
     sample = cv::Mat::eye(5000, 5000, CV_64FC1);
     timer.reset();
